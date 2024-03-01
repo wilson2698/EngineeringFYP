68f1b5333aba7896a55029ea0b6aff2c8b6fde7a
wilson2698 <wilson2698@gmail.com>
2024-03-01 16:36:42 +0800

    Moved misc functions

--- Codes/Ingestor.py
+++ Codes/Ingestor.py
@@ -1,49 +1,10 @@
 import pandas as pd
 import os
 
-from blockage_correction import maskell_blockage_correction, correct_blockage
-
+from misc_functions import get_data, get_coefficients, pct_change
 from airspeed import get_airspeeds
-
+from blockage_correction import maskell_blockage_correction, correct_blockage
 from charts import make_coeff_chart
-
-def get_data(filepath):
-    '''
-    This function serves to get the mean readings from the *.csv file output from the FT sensor.
-    Output is a pandas series
-    '''
-    df = pd.read_csv(filepath)
-    output = df[["Force X (N)","Force Y (N)", "Force Z (N)", "Torque X (N-m)", "Torque Y (N-m)", "Torque Z (N-m)"]].mean()
-    return output
-
-def get_coefficients(data, V):
-    '''
-    This function serves to calculate the CL and CD of the airfoil from the lift and drag forces from get_data()
-    '''
-    rho_air = 1.18 #kg/m^3
-    S = 32940.88019/(1000**2) #m^2 From solidworks
-    lift = data["Force X (N)"]
-    drag = data["Force Y (N)"]
-
-    ## 2L/(rho*V^2*S)
-    half_rho_v2_s = 0.5*rho_air*(V**2)*S
-    CL = lift/half_rho_v2_s
-    CD = drag/half_rho_v2_s ## Not Blockage Corrected Yet
-    return [CL, CD]
-
-def pct_change(df, base_col):
-    '''
-    Function here calculates the difference between the value and base value,
-    a negative sign shows a decrease while a positive sign is an increase
-    '''
-    out = pd.DataFrame()
-    out.index = df.index
-    for col in df:
-        if col == base_col:
-            continue
-        temp = ((df[col] - df[base_col])/abs(df[base_col]))*100
-        out[col] = temp
-    return out
 
 def ingest_experiment_set(folderpath):
     '''
---
+++ Codes/misc_functions.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def get_data(filepath):
+    '''
+    This function serves to get the mean readings from the *.csv file output from the FT sensor.
+    Output is a pandas series
+    '''
+    df = pd.read_csv(filepath)
+    output = df[["Force X (N)","Force Y (N)", "Force Z (N)", "Torque X (N-m)", "Torque Y (N-m)", "Torque Z (N-m)"]].mean()
+    return output
+
+def get_coefficients(data, V):
+    '''
+    This function serves to calculate the CL and CD of the airfoil from the lift and drag forces from get_data()
+    '''
+    rho_air = 1.18 #kg/m^3
+    S = 32940.88019/(1000**2) #m^2 From solidworks
+    lift = data["Force X (N)"]
+    drag = data["Force Y (N)"]
+
+    ## 2L/(rho*V^2*S)
+    half_rho_v2_s = 0.5*rho_air*(V**2)*S
+    CL = lift/half_rho_v2_s
+    CD = drag/half_rho_v2_s ## Not Blockage Corrected Yet
+    return [CL, CD]
+
+def pct_change(df, base_col):
+    '''
+    Function here calculates the difference between the value and base value,
+    a negative sign shows a decrease while a positive sign is an increase
+    '''
+    out = pd.DataFrame()
+    out.index = df.index
+    for col in df:
+        if col == base_col:
+            continue
+        temp = ((df[col] - df[base_col])/abs(df[base_col]))*100
+        out[col] = temp
+    return out
